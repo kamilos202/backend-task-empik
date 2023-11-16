b8f6f58679f5b14dede0361c2951f9aeb542cf46
Kamil <kamil.florowski@outlook.com>
2023-11-16 22:38:28 +0000

    Init the project

---
+++ src/test/java/com/example/backendtaskempik/BackendTaskEmpikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtaskempik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTaskEmpikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
